a41a61f2461cbc7dd647b41c201215c0a2f44852
mdalifkhandev <mdalifk2002@gmail.com>
2025-10-02 16:30:30 -0700

    test my project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl text-red-500">hello i am a student</h1>
-      <h1>hello i am a student</h1>
-      <h1>hello i am a student</h1>
-      <h1>hello i am a student</h1>
+      <h1>This is home page </h1>
+      <h1>it work tomoro</h1>
     </div>
   );
 }

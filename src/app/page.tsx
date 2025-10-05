383953ae6ea9a5c924c5b9212b0614bee061c564
mdalifkhandev <mdalifk2002@gmail.com>
2025-10-06 00:06:52 +0600

    login and sugnup page create

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div>
       <h1>This is home page </h1>
       <h1>it work tomoro</h1>
+
     </div>
   );
 }
